e5026024361bb7317333825efd780b6ebcf516b8
Blaine <blainenewvillage@gmail.com>
2026-01-25 18:43:07 +0400

    metadabase check attempt

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
     default: 'Acme Dashboard',
   },
   description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  metadataBase: new URL('https://nextjs-dashboard-4pj4.vercel.app'),
 };
 
 export default function RootLayout({
